caecc32cbc8f45049adb392d87b860325ae5a153
Ariane Morassi Sasso <ariane.sasso@gmail.com>
2015-05-21 23:16:16 -0300

    Melhorei os comentários

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,33 +11,74 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController {
 
+    /**
+     * Action that is executed before the user authentication. 
+     * This is needed so the user that is not active may see 
+     * the inactive users page
+     * 
+     * Ação que é executada antes da autenticação do usuário.
+     * Ela é necessária para que o usuário que não está ativo
+     * veja a página de usuários inativos
+     * 
+     * @param \Cake\Event\Event $event
+     * @return type
+     */
     public function beforeFilter(\Cake\Event\Event $event) {
         $this->Auth->allow(['inactiveUser']);
-        /**
-         * @TODO Verify if the next line is correct
-         */
+
         return parent::beforeFilter($event);
     }
 
     /**
+     * 
+     * @param type $user
+     * @return boolean
+     */
+//    public function isAuthorized($user) {
+//        $action = $this->request->params['action'];
+//        // The add and index actions are always allowed.
+//        if (in_array($action, ['login', 'logout'])) {
+//            return true;
+//        }
+//
+//        if ($user['id'] == 1) {
+//            return true;
+//        }
+//        return parent::isAuthorized($user);
+//    }
+
+    /**
      * Login method (using Auth component)
      * 
-     * @return void Redirects on successful login
+     * Método de Login (estamos usando o componente Auth do Cake)
+     * 
+     * @return void Redirects on successful login 
+     *              Redireciona em caso de sucesso no login
      */
     public function login() {
+        //The redirectUrl needs to be null so the login will always redirect 
+        //to the organzations action
+        //O atributo rirectUrl precisa ser null, assim o usuário semprre
+        //será redirecionado do login para a página de escolha de unidades
+        $this->Auth->redirectUrl(null);
         $this->layout = 'devoops_minimal';
+
         if ($this->request->is('post')) {
             $user = $this->Auth->identify();
+            //Redirects the user so he can choose the unity he wants to log into
+            //Redireciona o usuário para que ele possa escolher a unidade para logar
             if ($user) {
                 $this->Auth->setUser($user);
                 return $this->redirect($this->Auth->redirectUrl());
             }
             //Verifies whether the problem is the username, inactive user or password
+            //Verifica se o problema é o nome de usuário, usuário inativo ou a senha
             $users = $this->Users->findByUsername($this->request->data['username']);
-            //var_dump($users->first()->active);
+            //If the user does not exists // Se o usuário não existe
             if ($users->count() === 0) {
                 $this->Flash->bootstrapError('Seu nome de usuário está incorreto.');
-            } else if (!$users->first()->active) {
+                //If the user is not active
+            } else if (empty($users->first()->active)) {
                 return $this->redirect('usuario/sem-permissao');
             } else {
                 $this->Flash->bootstrapError('Senha incorreta.');
@@ -47,8 +88,10 @@ class UsersController extends AppController {
 
     /**
      * Logout method (using Auth component)
+     * Método de logout (estamos utilizando o componente Auth do Cake)
      * 
      * @return void Redirects on successful logout
+     *              Redireciona em caso de sucesso no logout
      */
     public function logout() {
         $this->Flash->bootstrapSuccess('Você encerrou sua sessão com sucesso.');
@@ -57,8 +100,10 @@ class UsersController extends AppController {
 
     /**
      * Displays a message for the inactive user
+     * Mostra uma mensagem para o usuário inativo
      * 
      * @return void Shows a static page
+     *              Exibe uma página estática
      */
     public function inactiveUser() {
         $this->layout = 'devoops_minimal';
@@ -83,7 +128,7 @@ class UsersController extends AppController {
      */
     public function view($id = null) {
         $user = $this->Users->get($id, [
-            'contain' => ['Users', 'Organizations', 'Permissions', 'Validities']
+            'contain' => ['Permissions', 'Organizations']
         ]);
         $this->set('user', $user);
         $this->set('_serialize', ['user']);
@@ -95,14 +140,17 @@ class UsersController extends AppController {
      * @return void Redirects on successful add, renders view otherwise.
      */
     public function add() {
+        $this->layout = 'ajax';
+            
         $user = $this->Users->newEntity();
         if ($this->request->is('post')) {
             $user = $this->Users->patchEntity($user, $this->request->data);
+            //pr($user);exit;
             if ($this->Users->save($user)) {
-                $this->Flash->success('The user has been saved.');
+                $this->Flash->bootstrapSuccess('O usuário foi salvo com sucesso.');
                 return $this->redirect(['action' => 'index']);
             } else {
-                $this->Flash->error('The user could not be saved. Please, try again.');
+                $this->Flash->bootstrapError('O usuário não pode ser salvo, por favor, tente novamente.');
             }
         }
         $this->set(compact('user'));
