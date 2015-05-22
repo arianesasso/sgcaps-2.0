685d51d7657f623958fee7968ff8fd9a985aec5a
Ariane Morassi Sasso <ariane.sasso@gmail.com>
2015-05-21 23:14:25 -0300

    Criei essa tabela utilizando bake e depois criei o finder customizado: findValidyOrganizations

---
+++ src/Model/Table/PermissionsTable.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Model\Table;
+
+use App\Model\Entity\Permission;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Permissions Model
+ */
+class PermissionsTable extends Table {
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config) {
+        $this->table('permissions');
+        $this->displayField('id');
+        $this->primaryKey('id');
+        $this->addBehavior('Timestamp');
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Organizations', [
+            'foreignKey' => 'organization_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Roles', [
+            'foreignKey' => 'role_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Users', [
+            'foreignKey' => 'admin_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator) {
+        $validator
+                ->add('id', 'valid', ['rule' => 'numeric'])
+                ->allowEmpty('id', 'create');
+
+        $validator
+                ->add('beginning', 'valid', ['rule' => 'datetime'])
+                ->requirePresence('beginning', 'create')
+                ->notEmpty('beginning');
+
+        $validator
+                ->add('ending', 'valid', ['rule' => 'datetime'])
+                ->allowEmpty('ending');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules) {
+        $rules->add($rules->existsIn(['user_id'], 'Users'));
+        $rules->add($rules->existsIn(['organization_id'], 'Organizations'));
+        $rules->add($rules->existsIn(['role_id'], 'Roles'));
+        $rules->add($rules->existsIn(['admin_id'], 'Users'));
+        return $rules;
+    }
+
+    /**
+     * Finds the Organizations in which the user has validy permissions
+     * Encontra as organizaćões nas quais o usuário tem permissões válidas
+     * 
+     * @param Query $query
+     * @param array $options
+     * @return type
+     */
+    public function findValidyOrganizations(Query $query, array $options) {
+        $fields = [
+            'Organizations.name',
+            'Organizations.id'
+        ];
+        return $this->find()
+                        ->select($fields)
+                        ->distinct($fields)
+                        ->where(['Permissions.user_id' => $options['user_id'], 'Permissions.ending >=' => date('Y-m-d H:i:s')])
+                        ->contain('Organizations');
+    }
+
+}
