496d5d3b2dde5fe15d06a7a6a6af46d45b0b5bb7
Ariane Morassi Sasso <ariane.sasso@gmail.com>
2015-06-15 01:14:43 -0300

    Agora, quando mudo o nome do usuário aqui, altero no menu superior também.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -185,6 +185,7 @@ class UsersController extends AppController {
             $userComplete = $this->Users->get($id);
             $user = $this->Users->patchEntity($userComplete, $this->request->data);
             if ($this->Users->save($user)) {
+                $this->request->session()->write('Auth.User.username', $this->request->data['username']);
                 $this->Flash->bootstrapSuccess('Os dados do usuário foram editados com sucesso.');
                 return $this->redirect(['action' => 'index']);
             } else {
