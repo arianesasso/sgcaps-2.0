105a3affda0da06b1503e614e3fcd58f6dc50ad0
Ariane Morassi Sasso <ariane.sasso@gmail.com>
2015-07-05 21:23:26 -0300

    Criei um validador de CPF

--- src/Model/Table/PeopleTable.php
+++ src/Model/Table/PeopleTable.php
@@ -75,11 +75,62 @@ class PeopleTable extends Table {
         $validator
                 ->add('birthdate', 'valid', ['rule' => ['date', 'dmy'], 'message' => 'Data inválida'])
                 ->allowEmpty('birthdate');
+        
+        $validator
+                ->add('cpf', 'custom', ['rule' => [$this,'validateCpf'], 'message' => 'CPF inválido'])
+                ->allowEmpty('cpf');
 
         $validator
                 ->allowEmpty('occupation');
 
         return $validator;
+    }
+    
+    /**
+     * Função para validar CPF segundo rotina de validação definida no site
+     * abaixo
+     * 
+     * @see http://www.geradorcpf.com/algoritmo_do_cpf.htm
+     * @param type $field O campo que está sendo validado
+     * @return boolean
+     */
+    public function validateCpf($field) {
+        //O campo vem com __ por causa da máscara do jquery
+        $cpf = str_replace(['.','-'], '', $field);
+        //Verifico se o CPF tem exatamente 11 dígitos
+        if ((strlen(trim($cpf))) != 11) {
+            return false;
+        }
+        
+        $digits = ['0','0'];
+        //Calcula o primeiro dígito de verificação.
+        $j = 0;
+        for($i = 10; $i >= 2; $i--) {
+            $digits[0] += $i*$cpf[$j];
+            $j++;
+        }
+        $digits[0] = $digits[0]%11;
+        if($digits[0] < 2) {
+            $digits[0] = 0;
+        } else {
+            $digits[0] = 11-$digits[0];
+        }
+
+        //Calcula o segundo dígito de verificação.
+        $j = 0;
+        for($i = 11; $i >= 3; $i--) {
+            $digits[1] += ($i)*$cpf[$j];
+            $j++;
+        }
+        $digits[1] += 2*$digits[0];
+        $digits[1] = $digits[1]%11;
+        if($digits[1] < 2) {
+            $digits[1] = 0;
+        } else {
+            $digits[1] = 11-$digits[1];
+        }
+        //Retorna Verdadeiro se os dígitos de verificação são os esperados.
+        return ($digits[0] == $cpf[9] && $digits[1] == $cpf[10]) ? true : false;
     }
 
     /**
