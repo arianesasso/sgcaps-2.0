e2120032df9efb3755c29d6d623bfe2f5fe234c4
Ariane Morassi Sasso <ariane.sasso@gmail.com>
2015-07-09 20:39:29 -0300

    Retornando o peoplecontroller, o qual eu deletei sem querer. O método addUser é utilizado no momento de se criar um usuário que é uma pessoa.

---
+++ src/Controller/PeopleController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * People Controller
+ *
+ * @property \App\Model\Table\PeopleTable $People
+ */
+class PeopleController extends AppController {
+
+    /**
+     * Adds user_id to the person
+     *
+     * @param string $id Person id.
+     * @param integer $userId User id.
+     * @return void Redirects on successful addition
+     */
+    public function addUser($id, $userId) {
+        if ($this->request->referer(true) != '/usuario/cadastrar') {
+            return $this->redirect(['controller' => 'usuario', 'action' => 'sem-permissao']);
+        }
+        $this->autoRender = false;
+        $people = $this->People->patchEntity($this->People->get($id), ['user_id' => $userId]);
+        if ($this->People->save($people)) {
+            $this->Flash->bootstrapSuccess('O usuário foi criado com sucesso.');
+            $controller = strtolower($this->request->params['controller']);
+            $this->redirect(['controller' => 'permissao', 'action' => 'adicionar', $userId, $controller]);
+        } else {
+            /**
+             * @TODO tratar o usuário que já foi inserido, fazer rollback (?)
+             */
+            $this->Flash->bootstrapError('Não foi possível criar um usuário para essa pessoa.');
+            $this->redirect(['controller' => 'usuario', 'action' => 'cadastrar']);
+        }
+    }
+
+}
