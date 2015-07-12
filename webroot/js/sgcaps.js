dab3b99c8b24ed80e5d55b44d5e0231205e3fead
Ariane Morassi Sasso <ariane.sasso@gmail.com>
2015-07-12 20:53:44 -0300

    Adicionando mensagens em pt-BR para o select2.

--- webroot/js/sgcaps.js
+++ webroot/js/sgcaps.js
@@ -132,7 +132,51 @@ $(document).ready(function () {
     $('#birthdate').change(validateAproxAge);
     
     // Funções para tratar o input de ocupação
-    $(".basic-select").select2({language: "pt"});
+    $(".basic-select").select2({
+        language: {
+            // Tradução de: https://github.com/select2/select2/blob/master/src/js/select2/i18n/pt-BR.js
+            errorLoading: function () {
+                return 'Os resultados não puderam ser carregados.';
+            },
+            inputTooLong: function (args) {
+                var overChars = args.input.length - args.maximum;
+
+                var message = 'Apague ' + overChars + ' caracter';
+
+                if (overChars != 1) {
+                    message += 'es';
+                }
+
+                return message;
+            },
+            inputTooShort: function (args) {
+                var remainingChars = args.minimum - args.input.length;
+
+                var message = 'Digite ' + remainingChars + ' ou mais caracteres';
+
+                return message;
+            },
+            loadingMore: function () {
+                return 'Carregando mais resultados…';
+            },
+            maximumSelected: function (args) {
+                var message = 'Você só pode selecionar ' + args.maximum + ' ite';
+
+                if (args.maximum == 1) {
+                    message += 'm';
+                } else {
+                    message += 'ns';
+                }
+
+                return message;
+            },
+            noResults: function () {
+                return 'Nenhum resultado encontrado';
+            },
+            searching: function () {
+                return 'Buscando…';
+            }}
+    });
     changeOccupationsInput();
     $('#not_found').click(changeOccupationsInput);
 
