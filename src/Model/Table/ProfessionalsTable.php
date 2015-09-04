17e77baa1103d7bf52c68cb706b72deb6d75fe50
Ariane Morassi Sasso <ariane.sasso@gmail.com>
2015-09-04 00:33:50 -0300

    Alterei este arquivo, pois agora o person_id está na tabela usuário

--- src/Model/Table/ProfessionalsTable.php
+++ src/Model/Table/ProfessionalsTable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Model\Table;
 
 use App\Model\Entity\Professional;
@@ -10,8 +11,7 @@ use Cake\Validation\Validator;
 /**
  * Professionals Model
  */
-class ProfessionalsTable extends Table
-{
+class ProfessionalsTable extends Table {
 
     /**
      * Initialize method
@@ -19,8 +19,7 @@ class ProfessionalsTable extends Table
      * @param array $config The configuration for the Table.
      * @return void
      */
-    public function initialize(array $config)
-    {
+    public function initialize(array $config) {
         $this->table('professionals');
         $this->displayField('id');
         $this->primaryKey('id');
@@ -39,19 +38,18 @@ class ProfessionalsTable extends Table
      * @param \Cake\Validation\Validator $validator Validator instance.
      * @return \Cake\Validation\Validator
      */
-    public function validationDefault(Validator $validator)
-    {
+    public function validationDefault(Validator $validator) {
         $validator
-            ->add('id', 'valid', ['rule' => 'numeric'])
-            ->allowEmpty('id', 'create');
-            
+                ->add('id', 'valid', ['rule' => 'numeric'])
+                ->allowEmpty('id', 'create');
+
         $validator
-            ->allowEmpty('board_acronym', 'valid', ['rule' => 'string', 'message' => 'Digite somente letras']);
-            
+                ->allowEmpty('board_acronym', 'valid', ['rule' => 'string', 'message' => 'Digite somente letras']);
+
         $validator
-            ->add('board_number', 'valid', ['rule' => 'alphaNumeric', 'message' => 'Digite somente números e letras'])
-            ->allowEmpty('board_number');
-      
+                ->add('board_number', 'valid', ['rule' => 'alphaNumeric', 'message' => 'Digite somente números e letras'])
+                ->allowEmpty('board_number');
+
         return $validator;
     }
 
@@ -62,16 +60,19 @@ class ProfessionalsTable extends Table
      * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
      * @return \Cake\ORM\RulesChecker
      */
-    public function buildRules(RulesChecker $rules)
-    {
+    public function buildRules(RulesChecker $rules) {
         $rules->add($rules->existsIn(['person_id'], 'People'));
         $rules->add($rules->existsIn(['board_state_id'], 'States'));
         return $rules;
     }
-       
+
     /**
-     * O método busca os profissionais que ainda não são usuários e que estão 
-     * vinculados atualmente a unidade na qual o usuário atual está logado
+     * Se o usuário logado for um 'gestor.geral' o método busca
+     * todos os profissionais que não são usuários ainda
+     * 
+     * Caso o gestor seja mais restrito o método busca os profissionais que 
+     * não são usuários e que estão vinculados à unidade na qual o usuário atual 
+     * está logado
      * 
      * @param Query $query
      * @param array $options
@@ -83,11 +84,17 @@ class ProfessionalsTable extends Table
             'People.name'
         ];
 
-        $condition[] = ['Professionals.person_id NOT IN' => $this->People->Users->find('all', ['fields' => 'person_id']), 
-                        'OrganizationsPeople.organization_id' => $options['organization_id'],
-                        'OR' => ['OrganizationsPeople.ended IS' => null,
-                        'OrganizationsPeople.ended >=' => date('Y-m-d H:i:s')]];
-        $matching = 'People.Organizations';
+        $condition[] = ['Professionals.person_id NOT IN' => $this->People->Users->find('all', 
+                                                            ['fields' => 'person_id',
+                                                             'conditions' => ['person_id IS NOT' => null]])];
+        $matching = 'People';
+
+        if (array_search('gestor.geral', $options['roles']) === false) {
+            $condition[] = ['OrganizationsPeople.organization_id' => $options['organization_id'],
+                           'OR' => ['OrganizationsPeople.ended IS' => null,
+                           'OrganizationsPeople.ended >=' => date('Y-m-d H:i:s')]];
+            $matching = 'People.Organizations';
+        }
 
         return $this->find()
                         ->select($fields)
